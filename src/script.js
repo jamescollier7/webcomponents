45bc2c9b5c391c4e8d422d894a0d009767adbd6f
James Collier <jamescollier7@users.noreply.github.com>
2023-04-27 09:54:09 -0600

    Update script.js

--- src/script.js
+++ src/script.js
@@ -1,8 +1,30 @@
+// web component
 class HelloWorld extends HTMLElement {
-
+  
+  // because this extends HTMLElement, call super() first
+  // then do other initializations
+  constructor() {
+    super();
+    this.name = 'World';
+  }
+  
   // connect component
   connectedCallback() {
-    this.textContent = 'Hello World!';
+    this.textContent = `Hello ${ this.name }!`;
   }
-
+  
+  // component attributes
+  static get observedAttributes() {
+    return ['name'];
+  }
+  
+  // attribute change
+  attributeChangedCallback(property, oldValue, newValue) {
+    if (oldValue === newValue) return;
+    this[ property ] = newValue;
+  }
+  
 }
+
+// register component
+customElements.define( 'hello-world', HelloWorld );
